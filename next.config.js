b1bffe249f619a23096889aa282bcba81336548c
Danilo JR3 Panaro <danilo.panaro@jr3.it>
2020-11-02 20:48:45 +0100

    2011022040 - ARIA Labels Fix

--- next.config.js
+++ next.config.js
@@ -9,6 +9,19 @@ module.exports = withPWA({
     dest: "public",
     // An array of glob pattern strings to exclude files in the public folder from being precached.
     publicExcludes: ["!video/UrbanoRurale_720p.webm"],
+    runtimeCaching: [
+      {
+        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "static-image-assets",
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 365 * (24 * 60 * 60), // 365 * 24 hours
+          },
+        },
+      },
+    ],
   },
   images: {
     domains: ["api.agritettura.org"],
